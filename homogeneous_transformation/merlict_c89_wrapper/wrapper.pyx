592dd4477a365fe03fdbf865714826b2e8b9c861
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2020-08-13 15:10:56 +0200

    explicit parameters

--- homogeneous_transformation/merlict_c89_wrapper/wrapper.pyx
+++ homogeneous_transformation/merlict_c89_wrapper/wrapper.pyx
@@ -15,13 +15,11 @@ cdef extern from "merlict_c89/merlict_c89/mliVec.h":
     )
 
 cdef extern from "merlict_c89/merlict_c89/mliRay.h":
-    struct mliRay:
-        pass
+    pass
 
 
 cdef extern from "merlict_c89/merlict_c89/mliRotMat.h":
-    struct mliRotMat:
-        pass
+    pass
 
 
 cdef extern from "merlict_c89/merlict_c89/mliQuaternion.h":
@@ -86,8 +84,8 @@ Quaternion = namedtuple('Quaternion', ['w', 'x', 'y', 'z'])
 HomTra = namedtuple('HomTra', ['trans', 'rot'])
 Vec = namedtuple('Vec', ['x', 'y', 'z'])
 
-def Quaternion_set_tait_bryan(x, y, z):
-    q = mliQuaternion_set_tait_bryan(x, y, z)
+def Quaternion_set_tait_bryan(x_angle, y_angle, z_angle):
+    q = mliQuaternion_set_tait_bryan(x_angle, y_angle, z_angle)
     return Quaternion(q.w, q.x, q.y, q.z)
 
 
@@ -104,24 +102,30 @@ def Quaternion_set_unit_xyz(x, y, z):
     return Quaternion(q.w, q.x, q.y, q.z)
 
 
-def sequence(a, b):
-    _a = mliHomTraComp_set(
-        mliVec_set(a.trans.x, a.trans.y, a.trans.z),
-        mliQuaternion_set(a.rot.w, a.rot.x, a.rot.y, a.rot.z)
+def sequence(homtra_A_to_B, homtra_B_to_C):
+    """
+    Return the homogenous transformation (a_to_d) by estimating the sequence
+    along (a_to_b) and (b_to_d).
+    """
+    ab = homtra_A_to_B
+    bc = homtra_B_to_C
+    _ab = mliHomTraComp_set(
+        mliVec_set(ab.trans.x, ab.trans.y, ab.trans.z),
+        mliQuaternion_set(ab.rot.w, ab.rot.x, ab.rot.y, ab.rot.z)
     )
-    _b = mliHomTraComp_set(
-        mliVec_set(b.trans.x, b.trans.y, b.trans.z),
-        mliQuaternion_set(b.rot.w, b.rot.x, b.rot.y, b.rot.z)
+    _bc = mliHomTraComp_set(
+        mliVec_set(bc.trans.x, bc.trans.y, bc.trans.z),
+        mliQuaternion_set(bc.rot.w, bc.rot.x, bc.rot.y, bc.rot.z)
     )
-    _c = mliHomTraComp_sequence(_a, _b)
+    _ac = mliHomTraComp_sequence(_ab, _bc)
     return HomTra(
-        Quaternion(_c.rot.w, _c.rot.x, _c.rot.y, _c.rot.z),
-        Vec(_c.trans.x, _c.trans.y, _c.trans.z)
+        rot=Quaternion(_ac.rot.w, _ac.rot.x, _ac.rot.y, _ac.rot.z),
+        trans=Vec(_ac.trans.x, _ac.trans.y, _ac.trans.z)
     )
 
 
 cdef mliHomTra _extract_homtra(homtra):
-    H_comp = mliHomTraComp_set(
+    h_comp = mliHomTraComp_set(
         mliVec_set(
             homtra.trans.x,
             homtra.trans.y,
@@ -134,41 +138,56 @@ cdef mliHomTra _extract_homtra(homtra):
             homtra.rot.z,
         )
     )
-    H = mliHomTra_from_compact(H_comp)
-    return H
+    return mliHomTra_from_compact(h_comp)
 
 
-def transform_position(homtra, position):
-    H = _extract_homtra(homtra)
-    v = mliHomTra_pos(
-        &H,
-        mliVec_set(position[0], position[1], position[2])
+def transform_position(homtra_A_to_B, position_wrt_A):
+    """
+    returns position w.r.t. B.
+    """
+    pA = position_wrt_A
+    _homtra_AB = _extract_homtra(homtra_A_to_B)
+    _pB = mliHomTra_pos(
+        &_homtra_AB,
+        mliVec_set(pA[0], pA[1], pA[2])
     )
-    return [v.x, v.y, v.z]
+    return [_pB.x, _pB.y, _pB.z]
 
 
-def transform_position_inverse(homtra, position):
-    H = _extract_homtra(homtra)
-    v = mliHomTra_pos_inverse(
-        &H,
-        mliVec_set(position[0], position[1], position[2])
+def transform_position_inverse(homtra_A_to_B, position_wrt_B):
+    """
+    returns position w.r.t. A.
+    """
+    pB = position_wrt_B
+    _homtra_AB = _extract_homtra(homtra_A_to_B)
+    _pA = mliHomTra_pos_inverse(
+        &_homtra_AB,
+        mliVec_set(pB[0], pB[1], pB[2])
     )
-    return [v.x, v.y, v.z]
+    return [_pA.x, _pA.y, _pA.z]
 
 
-def transform_direction(homtra, direction):
-    H = _extract_homtra(homtra)
-    v = mliHomTra_dir(
-        &H,
-        mliVec_set(direction[0], direction[1], direction[2])
+def transform_direction(homtra_A_to_B, direction_wrt_A):
+    """
+    returns direction w.r.t. A.
+    """
+    dA = direction_wrt_A
+    _homtra_AB = _extract_homtra(homtra_A_to_B)
+    _dB = mliHomTra_dir(
+        &_homtra_AB,
+        mliVec_set(dA[0], dA[1], dA[2])
     )
-    return [v.x, v.y, v.z]
+    return [_dB.x, _dB.y, _dB.z]
 
 
-def transform_direction_inverse(homtra, direction):
-    H = _extract_homtra(homtra)
-    v = mliHomTra_dir_inverse(
-        &H,
-        mliVec_set(direction[0], direction[1], direction[2])
+def transform_direction_inverse(homtra_A_to_B, direction_wrt_B):
+    """
+    returns direction w.r.t. B.
+    """
+    dB = direction_wrt_B
+    _homtra_AB = _extract_homtra(homtra_A_to_B)
+    _dA = mliHomTra_dir_inverse(
+        &_homtra_AB,
+        mliVec_set(dB[0], dB[1], dB[2])
     )
-    return [v.x, v.y, v.z]
+    return [_dA.x, _dA.y, _dA.z]
